e1222cd3ba779a3136370e47f394b39273faf4d3
Nick Pham <hebenzudoaa@gmail.com>
2024-04-21 22:00:14 -0400

    switched to regex

--- extractor.py
+++ extractor.py
@@ -1,24 +1,27 @@
-f= open("./battle_log/pokellmon_vs_invited_player/literally an ai - battle-gen8ou-45.html", "r")
 
+
+import re
+
+f= open("./battle_log/pokellmon_vs_invited_player/literally an ai - battle-gen8ou-45.html", "r")
 player="p1"
 turn_count=0
 attack_count=0
 switch_count=-1
 with f as openfileobject:
     for line in openfileobject:
-        line = line.split('|')
-        if len(line)>1:
-            if line[1]=="player":
-                if line[3]=="literally an ai":
-                    player = line[2]
-            if line[1]=="turn":
-                turn_count+=1;
-            if line[1]=="move":
-                if player in line[2]:
-                    attack_count+=1
-            if line[1]=="switch":
-                if player in line[2]:
-                    switch_count+=1
+        if bool(re.search('literally an ai', line)) and bool(re.search('player', line)):
+            if bool(re.search('p1', line)):
+                player="p1"
+            elif bool(re.search('p2', line)):
+                player="p2"
+        if bool(re.search('\|turn\|', line)):
+            turn_count+=1
+        if bool(re.search(player,line)):
+            if bool(re.search("\|move\|"+player, line)):
+                print(line)
+                attack_count+=1
+            elif bool(re.search("\|switch\|", line)):
+                switch_count+=1
 
 print("Turns: "+ str(turn_count))
 print("Did a move "+str(attack_count)+" times")
