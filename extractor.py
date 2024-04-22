abde144dda7b5323d7042b16b7c4667af1297955
Nick Pham <hebenzudoaa@gmail.com>
2024-04-21 20:58:31 -0400

    attack and switch rates

--- extractor.py
+++ extractor.py
@@ -2,13 +2,26 @@ f= open("./battle_log/pokellmon_vs_invited_player/literally an ai - battle-gen8o
 
 player="p1"
 turn_count=0
+attack_count=0
+switch_count=-1
 with f as openfileobject:
     for line in openfileobject:
-        print(line.split('|'))
-        if line[1]=="player":
-            if line[3]=="literally an ai":
-                player = line[2]
-        if line[1]=="turn":
-            turn_count+=1;
+        line = line.split('|')
+        if len(line)>1:
+            if line[1]=="player":
+                if line[3]=="literally an ai":
+                    player = line[2]
+            if line[1]=="turn":
+                turn_count+=1;
+            if line[1]=="move":
+                if player in line[2]:
+                    attack_count+=1
+            if line[1]=="switch":
+                if player in line[2]:
+                    switch_count+=1
 
-        
+print("Turns: "+ str(turn_count))
+print("Did a move "+str(attack_count)+" times")
+print(str(attack_count/turn_count))
+print("Switched "+str(switch_count)+" times")
+print(str(switch_count/turn_count))
