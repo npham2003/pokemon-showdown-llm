bc2a5319df43aa2d7669bbcf3f5527d4b50858ff
Nick Pham <hebenzudoaa@gmail.com>
2024-04-25 12:14:45 -0400

    dictionary with base

--- extractor.py
+++ extractor.py
@@ -22,8 +22,12 @@ for line in f:
         name_raw = line[9:]
         name_raw = name_raw.replace('|','').strip()
         name = name_raw.split(',')[0].strip()
-        name = name.split('-')[0].strip() # Unsure
-        pokemons[name]=1
+        base_form = name.split('-')[0].strip() # Unsure
+        pokemons[name]={
+            "base": base_form,
+            "health": 1
+        }
+
 
     if bool(re.search('\|turn\|', line)):
         turn_count+=1
@@ -32,21 +36,22 @@ for line in f:
             attack_count+=1
         elif bool(re.search("\|switch\|", line)):
             switch_count+=1
-    if re.search('|-damage|',line) and re.search(player,line):
+    if re.search('\|-damage\|',line) and re.search(player,line):
         if re.search('0 fnt',line):  # Pokemon is dead
             pokemon_cnt -= 1
             for pokemon_name in pokemons.keys():
-                if re.search(pokemon_name,line):
-                    pokemons[pokemon_name]= 0
+                if re.search(pokemons[pokemon_name]["base"],line):
+                    pokemons[pokemon_name]["health"]= 0
                     #print(pokemon_name,'Dead')
         if re.search('[\d]{1,3}\/[\d]{1,3}',line):
+            
             damage_cnt +=1
             text=re.findall('[\d]{1,3}\/[\d]{1,3}',line)[0]
             num,dem = text.split('/')
             perc=int(num)/int(dem)
             for pokemon_name in pokemons.keys():
-                if re.search(pokemon_name,line):
-                    pokemons[pokemon_name]= perc
+                if re.search(pokemons[pokemon_name]["base"],line):
+                    pokemons[pokemon_name]["health"]= perc
                     #print(pokemon_name,perc)
             
 
@@ -56,5 +61,9 @@ print(str(attack_count/turn_count))
 print("Switched "+str(switch_count)+" times")
 print(str(switch_count/turn_count))
 print(pokemons)
-print('Average Health {:.2f}%'.format(sum(list(pokemons.values())*100)/len(pokemons)))
+sum_health=0
+for pokemon_name in pokemons.keys():
+    sum_health+=pokemons[pokemon_name]["health"]
+average=sum_health*100/len(pokemons)
+print('Average Health {:.2f}%'.format(average))
 print('Number of Pokemon',pokemon_cnt)
