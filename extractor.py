fef4cbcbe22c5c773016b9d36d7eb7b62a4185ec
Divyansh Agarwal <57028983+divyansh7877@users.noreply.github.com>
2024-04-28 16:12:47 -0400

    Added average of metrics, and record in CSV

--- extractor.py
+++ extractor.py
@@ -1,67 +1,96 @@
 import re
-
-f= open("./battle_log/pokellmon_vs_invited_player/literally an ai - battle-gen8ou-43.html", "r")
-player="p1"
-turn_count=0
-attack_count=0
-switch_count=-1
-pokemon_cnt = 6
-avg_hlth = 100
-damage_cnt = 0
-pokemons={}
+import os
+import pandas as pd
 
 
-for line in f:
-    if bool(re.search('literally an ai', line)) and bool(re.search('player', line)):
-        if bool(re.search('p1', line)):
-            player="p1"
-        elif bool(re.search('p2', line)):
-            player="p2"
 
-    if re.search('\|poke\|',line) and re.search(player,line):  # Get pokemon names
-        name_raw = line[9:]
-        name_raw = name_raw.replace('|','').strip()
-        name = name_raw.split(',')[0].strip()
-        base = name.split('-')[0].strip() # Unsure
-        pokemons[name]={'base_form':base,
-                        'HP':1}
+def metric_calculator(f,filename):
+    '''
+    
+    
+    '''
+    player="p1"
+    turn_count=0
+    attack_count=0
+    switch_count=-1
+    pokemon_cnt = 6
+    damage_cnt = 0
+    pokemons={}
+
+    for line in f:
+        if bool(re.search('literally an ai', line)) and bool(re.search('player', line)):
+            if bool(re.search('p1', line)):
+                player="p1"
+            elif bool(re.search('p2', line)):
+                player="p2"
+
+        if re.search('\|poke\|',line) and re.search(player,line):  # Get pokemon names
+            name_raw = line[9:]
+            name_raw = name_raw.replace('|','').strip()
+            name = name_raw.split(',')[0].strip()
+            base = name.split('-')[0].strip() # Unsure
+            pokemons[name]={'base_form':base,
+                            'HP':1}
 
 
-    if bool(re.search('\|turn\|', line)):
-        turn_count+=1
-    if bool(re.search(player,line)):
-        if bool(re.search("\|move\|"+player, line)):
-            attack_count+=1
-        elif bool(re.search("\|switch\|", line)):
-            switch_count+=1
-    if re.search('\|-damage\|',line) and re.search(player,line):
-        if re.search('0 fnt',line):  # Pokemon is dead
-            pokemon_cnt -= 1
-            for pokemon_name,tdic in pokemons.items():
-                base = tdic['base_form']
-                if re.search(pokemon_name,line) or re.search(base,line):
-                    pokemons[pokemon_name]= {'base_form':base,
-                                            'HP': 0}
-        if re.search('[\d]{1,3}\/[\d]{1,3}',line):
-            
-            damage_cnt +=1
-            text=re.findall('[\d]{1,3}\/[\d]{1,3}',line)[0]
-            num,dem = text.split('/')
-            perc=int(num)/int(dem)
-            for pokemon_name,tdic in pokemons.items():
-                base = tdic['base_form']
-                if re.search(pokemon_name,line) or re.search(base,line):
-                    pokemons[pokemon_name]= {'base_form':base,
-                                            'HP':perc}
+        if bool(re.search('\|turn\|', line)):
+            turn_count+=1
+        if bool(re.search(player,line)):
+            if bool(re.search("\|move\|"+player, line)):
+                attack_count+=1
+            elif bool(re.search("\|switch\|", line)):
+                switch_count+=1
+        if re.search('\|-damage\|',line) and re.search(player,line):
+            if re.search('0 fnt',line):  # Pokemon is dead
+                pokemon_cnt -= 1
+                for pokemon_name,tdic in pokemons.items():
+                    base = tdic['base_form']
+                    if re.search(pokemon_name,line) or re.search(base,line):
+                        pokemons[pokemon_name]= {'base_form':base,
+                                                'HP': 0}
+            if re.search('[\d]{1,3}\/[\d]{1,3}',line):
+                
+                damage_cnt +=1
+                text=re.findall('[\d]{1,3}\/[\d]{1,3}',line)[0]
+                num,dem = text.split('/')
+                perc=int(num)/int(dem)
+                for pokemon_name,tdic in pokemons.items():
+                    base = tdic['base_form']
+                    if re.search(pokemon_name,line) or re.search(base,line):
+                        pokemons[pokemon_name]= {'base_form':base,
+                                                'HP':perc}
+    if len(pokemons) == 0:
+        avg_hp = 0
+    else:
+        avg_hp = sum([tdic['HP'] for tdic in pokemons.values()])/len(pokemons)
 
-            
+    
+    return [turn_count,attack_count,switch_count,avg_hp*100,pokemons,filename]
 
-print("Turns: "+ str(turn_count))
-print("Did a move "+str(attack_count)+" times")
-print(str(attack_count/turn_count))
-print("Switched "+str(switch_count)+" times")
-print(str(switch_count/turn_count))
-print(pokemons)
-avg_hp = sum([tdic['HP'] for tdic in pokemons.values()])/len(pokemons)
-print('Average Health {:.2f}%'.format(avg_hp*100))
-print('Number of Pokemon',pokemon_cnt)
+
+df = pd.DataFrame(columns = ['Turn Count','Attack Count','Switch Count','Average HP','Pokemons','File'])
+
+for file in os.listdir("battle_log/pokellmon_vs_invited_player"):
+    if file.endswith(".html") and re.search('literally an ai',file):
+        filename=os.path.join("battle_log/pokellmon_vs_invited_player", file)
+        f =open(filename)
+        metric = metric_calculator(f,filename)
+        df.loc[len(df.index)] = metric
+        f.close()
+
+df.to_csv('battle_metrics.csv',index=None)
+lost= df['Average HP'].isin([0]).sum(axis=0)
+win_rate =lost /len(df)
+print('-----Metrics--------')
+print('Battles Lost:\t\t',lost)
+print('Battles Won:\t\t',len(df) - lost)
+print('Total Battles:\t\t',len(df))
+print('Win Rate: \t\t {:.2f}%'.format(win_rate*100))
+print('Average Turn Count: \t {:.2f}'.format(df['Turn Count'].mean()))
+print('Average Attack Count: \t {:.2f}'.format(df['Attack Count'].mean()))
+print('Average Switch Count: \t {:.2f}'.format(df['Switch Count'].mean()))
+print('Average HP: \t\t {:.2f}%'.format(df['Average HP'].mean()))
+
+
+
+
